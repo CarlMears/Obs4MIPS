740bc6b8f78901b98a0ab6d0d259871e5fd53f67
Carl Mears <mears@remss.com>
2026-03-13 17:35:51 -0700

    debugging

--- calc_tb_sounder/src/rtm_tables_AMSU.f90
+++ calc_tb_sounder/src/rtm_tables_AMSU.f90
@@ -445,6 +445,97 @@ contains
         return
 
     end subroutine read_sea_ice_emiss_table_AMSU
+
+    subroutine read_sea_ice_emiss_table_AMSU_netcdf(amsu_channel,path_to_data,err)
+
+        implicit none
+
+        integer(4),intent(in)                 :: amsu_channel
+        character(len = *), intent(in)        :: path_to_data
+        integer(4),intent(out)                :: err
+        character(len = 200)                  :: file
+        integer(4)                            :: ncid,varid
+        integer(4)                            :: status
+        integer(4)                            :: t_index,fov_index
+        real(4),dimension(0:num_t)            :: t_vals
+        real(4),dimension(0:num_fov-1)        :: fov_vals
+        real(4),dimension(0:num_fov-1,0:num_t) :: sea_ice_emiss_netcdf
+
+        write(file,100) trim(path_to_data),amsu_channel
+100     format(a,'/emiss_tables/amsu_',i2.2,'_emiss_table_sea_ice.nc')
+
+        print *, "Reading AMSU sea ice emissivity table from netCDF file: "
+        print *, trim(file)
+
+        status = nf90_open(trim(file), nf90_nowrite, ncid)
+        if (status /= nf90_noerr) then
+            print *, nf90_strerror(status)
+            err = status
+            return
+        endif
+
+        status = nf90_inq_varid(ncid, 'temperature', varid)
+        if (status /= nf90_noerr) then
+            err = status
+            print *, nf90_strerror(status)
+            status = nf90_close(ncid)
+            return
+        endif
+        status = nf90_get_var(ncid, varid, t_vals)
+        if (status /= nf90_noerr) then
+            err = status
+            print *, nf90_strerror(status)
+            status = nf90_close(ncid)
+            return
+        endif
+
+        status = nf90_inq_varid(ncid, 'fov', varid)
+        if (status /= nf90_noerr) then
+            err = status
+            print *, nf90_strerror(status)
+            status = nf90_close(ncid)
+            return
+        endif
+        status = nf90_get_var(ncid, varid, fov_vals)
+        if (status /= nf90_noerr) then
+            err = status
+            print *, nf90_strerror(status)
+            status = nf90_close(ncid)
+            return
+        endif
+
+        status = nf90_inq_varid(ncid, 'emissivity', varid)
+        if (status /= nf90_noerr) then
+            err = status
+            print *, nf90_strerror(status)
+            status = nf90_close(ncid)
+            return
+        endif
+        status = nf90_get_var(ncid, varid, sea_ice_emiss_netcdf)
+        if (status /= nf90_noerr) then
+            err = status
+            print *, nf90_strerror(status)
+            status = nf90_close(ncid)
+            return
+        endif
+
+        status = nf90_close(ncid)
+
+        T0_sea_ice = t_vals(0)
+        Delta_t_sea_ice = t_vals(1) - t_vals(0)
+
+        ! NetCDF uses (fov, temperature); map fov index 0..14 -> 1..15
+        do t_index = 0, num_t
+            do fov_index = 0, num_fov-1
+                sea_ice_emiss_table(t_index,fov_index+1) = sea_ice_emiss_netcdf(fov_index,t_index)
+            end do
+        end do
+
+        amsu_channel_loaded_emiss = amsu_channel
+        err = 0
+        return
+
+    end subroutine read_sea_ice_emiss_table_AMSU_netcdf
     
      
     real(4) function find_abs_q_AMSU(t,p,q,error)
